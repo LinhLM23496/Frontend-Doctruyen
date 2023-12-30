b38babf1d6cdcefb2da306798badafdb167960ad
le linh <linhlm23496@gmail.com>
2023-12-31 00:49:07 +0700

    fix component list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,10 +1,25 @@
 import React, { Ref, forwardRef } from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, Image, StyleSheet } from 'react-native'
 import { ListProps } from './types'
+import images from 'assets/images'
+import { avatarSize } from 'themes'
 
 const List = forwardRef((props: ListProps<any>, ref?: Ref<FlatList<any>>) => {
   const { Element = FlatList, ...rest } = props
-  return <Element ref={ref} {...rest} />
+
+  const renderEmptyState = () => {
+    return <Image source={images.empty} style={styles.empty} />
+  }
+
+  return <Element ref={ref} ListEmptyComponent={renderEmptyState} {...rest} />
 })
 
 export default List
+
+const styles = StyleSheet.create({
+  empty: {
+    alignSelf: 'center',
+    height: avatarSize.xxl,
+    aspectRatio: 1
+  }
+})
